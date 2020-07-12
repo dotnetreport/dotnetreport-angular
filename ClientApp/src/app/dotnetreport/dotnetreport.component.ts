d02ddf61e618056641439aeffdf272ecd3eb82c6
Razi Syed <syed_razi@hotmail.com>
2020-07-12 18:07:58 -0500

    added barebones dotnet report angular component

---
+++ ClientApp/src/app/dotnetreport/dotnetreport.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dotnetreport',
+  templateUrl: './dotnetreport.component.html',
+})
+export class DotNetReportComponent {
+}
--- Startup.cs
+++ Startup.cs
@@ -22,7 +22,9 @@ namespace Angular.ReportBuilder
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options => options.UseMemberCasing());
+
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
